5ff41b4fa33ed350c6436b68f95b669ea3e6e883
Abigail Bentley <sgabentl@liverpool.ac.uk>
2024-11-22 14:08:31 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import paho.mqtt.subscribe as subscribe
 import pandas as pd
-import datetime as dt
+import time
+from datetime import datetime
 
-NUM_OF_READINGS = 50
+NUM_OF_READINGS = 5
 MEASUREMENT_TITLES = ['aitime', 'aitemperature', 'aihumidity', 'aigas', 'aibco2']
 
 def get_experiment_number():
@@ -20,26 +21,31 @@ def get_experiment_number():
 
 
 # Create filename id:
-date_now = dt.datetime.now()
+date_now = datetime.now()
 date_str = date_now.strftime('%d%m%Y')
 nicla_ID = "n1"
 mkr_ID = "m1"
 exp_nr = str(get_experiment_number())
 filename_csv = mkr_ID + nicla_ID + date_str + "-" + exp_nr
 
+
 topics = MEASUREMENT_TITLES
 data_table = []
-print("begining for loop")
-for _ in range(0, (NUM_OF_READINGS+1), 1):
+start_time = time.time()
+for _ in range(0, (NUM_OF_READINGS + 1), 1):
     data = []
     m = subscribe.simple(topics, hostname="pf-eveoxy0ua6xhtbdyohag.cedalo.cloud", retained=False, msg_count=len(topics))
-    print("m subscribed")
+    elapsed_time = time.time() - start_time
+
     for a in m:
             data.append(float(a.payload))
+
+    print("reading no.", _)
     print(data)
+    print(elapsed_time)
 
     measurements = {}
-    measurements["time"] = data[0]
+    measurements["time"] = round(elapsed_time, 1)
     measurements["temperature"] = data[1]
     measurements["humidity"] = data[2]
     measurements["gas"] = data[3]
